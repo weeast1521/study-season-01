184814a437f64071fe858aaa50d4c915d482dcea
kwonjeong9470 <kwonjeong9470@gmail.com>
2026-02-24 10:32:03 +0900

    권정: 3주차 과제 제출

--- 03 우선순위큐 (Priority Queue)/권정/BOJ_1655_가운데를말해요.java
+++ 03 우선순위큐 (Priority Queue)/권정/BOJ_1655_가운데를말해요.java
@@ -7,18 +7,25 @@ public class BOJ_1655_가운데를말해요 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        // O(1)
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
+        // N * O(log N)
+        // -> O(N log N)
         int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
+            // O(1)
             int num = Integer.parseInt(br.readLine());
+            // k -> 힙 원소
+            // O(log k)
             if (maxHeap.size() == minHeap.size()) {
                 maxHeap.add(num);
             } else {
                 minHeap.add(num);
             }
 
+            // O(log k)
             if (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
                 if (maxHeap.peek() > minHeap.peek()) {
                     int max = maxHeap.poll();
@@ -28,6 +35,7 @@ public class BOJ_1655_가운데를말해요 {
                 }
             }
 
+            // O(1)
             if (!maxHeap.isEmpty()) {
                 System.out.println(maxHeap.peek());
             }
